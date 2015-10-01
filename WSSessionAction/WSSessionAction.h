6fabce40453b84436ec314b353e728ab466c2425
Riley Crebs <rcrebs@salesforce.com>
2015-10-01 13:28:47 -0700

    more updateing

--- WSSessionAction/WSSessionAction.h
+++ WSSessionAction/WSSessionAction.h
@@ -8,13 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@protocol WSActionProtocol;
-
-typedef void (^WSAPICompletionBlock)(id response);
-typedef void (^WSAPIFailureBlock)(NSError* error);
-typedef void (^WSAPICancelBlock)();
-typedef void (^WSAPIAuthenticateBlock)(id<WSActionProtocol> action, WSAPICompletionBlock completionBlock, WSAPIFailureBlock failureBlock);
-
 //! Project version number for WSSessionAction.
 FOUNDATION_EXPORT double WSSessionActionVersionNumber;
 
@@ -23,5 +16,6 @@ FOUNDATION_EXPORT const unsigned char WSSessionActionVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <WSSessionAction/PublicHeader.h>
 #import <WSSessionAction/WSSessionTaskDataOperation.h>
-#import <WSSessionActoin/WSActionProtocol.h>
+#import <WSSessionAction/WSActionProtocol.h>
+#import <WSSessionAction/WSAPIConstants.h>
 
